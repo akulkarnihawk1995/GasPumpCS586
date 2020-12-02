4964442c40f587508a3d47d52367132531f0bef9
akshay <akulkarni10@hawk.iit.edu>
2020-12-01 20:20:49 -0600

    AbstarctFactories and Concrete Factorys

--- src/main/AbstractFactory.java
+++ src/main/AbstractFactory.java
@@ -1,5 +1,23 @@
 package main;
 
-public class AbstractFactory {
+public abstract class AbstractFactory {
     DataStore ds;
+    public abstract StorePrices Create_StorePrices();
+    public abstract PayMsg Create_PayMsg();
+    public abstract StoreCash Create_StoreCash();
+    public abstract DisplayMenu Create_DisplayMenu();
+    public abstract RejectMsg Create_RejectMsg();
+    public abstract SetPrice Create_SetPrice();
+    public abstract ReadyMsg Create_ReadyMsg();
+    public abstract SetInitialValues Create_SetInitialValues();
+    public abstract PumpGasUnit Create_PumpGasUnit();
+    public abstract GasPumpedMsg Create_GasPumpedMsg();
+    public abstract CancelMsg Create_CancelMsg();
+    public abstract ReturnCash Create_ReturnCash();
+    public abstract WrongPinMsg Create_WrongPinMsg();
+    public abstract StorePin Create_StorePin();
+    public abstract EnterPinMsg Create_EnterPinMsg();
+    public abstract SetW Create_SetW();
+    public abstract EjectCard Create_EjectCard();
+    public abstract DataStore get_DataStore();
 }
---
+++ src/main/ConcreteFactory1.java
@@ -0,0 +1,94 @@
+package main;
+
+public class ConcreteFactory1 extends AbstractFactory{
+
+    @Override
+    public StorePrices Create_StorePrices() {
+        return null;
+    }
+
+    @Override
+    public PayMsg Create_PayMsg() {
+        return null;
+    }
+
+    @Override
+    public StoreCash Create_StoreCash() {
+        return null;
+    }
+
+    @Override
+    public DisplayMenu Create_DisplayMenu() {
+        return null;
+    }
+
+    @Override
+    public RejectMsg Create_RejectMsg() {
+        return null;
+    }
+
+    @Override
+    public SetPrice Create_SetPrice() {
+        return null;
+    }
+
+    @Override
+    public ReadyMsg Create_ReadyMsg() {
+        return null;
+    }
+
+    @Override
+    public SetInitialValues Create_SetInitialValues() {
+        return null;
+    }
+
+    @Override
+    public PumpGasUnit Create_PumpGasUnit() {
+        return null;
+    }
+
+    @Override
+    public GasPumpedMsg Create_GasPumpedMsg() {
+        return null;
+    }
+
+    @Override
+    public CancelMsg Create_CancelMsg() {
+        return null;
+    }
+
+    @Override
+    public ReturnCash Create_ReturnCash() {
+        return null;
+    }
+
+    @Override
+    public WrongPinMsg Create_WrongPinMsg() {
+        return null;
+    }
+
+    @Override
+    public StorePin Create_StorePin() {
+        return null;
+    }
+
+    @Override
+    public EnterPinMsg Create_EnterPinMsg() {
+        return null;
+    }
+
+    @Override
+    public SetW Create_SetW() {
+        return null;
+    }
+
+    @Override
+    public EjectCard Create_EjectCard() {
+        return null;
+    }
+
+    @Override
+    public DataStore get_DataStore() {
+
+    }
+}
