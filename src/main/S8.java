46b653e7b30e5dbdad2a73f011144297d33f7757
akshay <akulkarni10@hawk.iit.edu>
2020-12-04 02:07:56 -0600

    Add CorrectPin() and Complete S8

--- src/main/S8.java
+++ src/main/S8.java
@@ -4,67 +4,70 @@ public class S8 extends State{
 
     @Override
     public void Activate() {
-
+        System.out.println("Invalid State. Currently on State S8. Please Start");
     }
 
     @Override
     public void Start() {
-
+        System.out.println("Invalid State. Currently on State S8. Please Start");
     }
 
     @Override
     public void PayDebit() {
-
+        System.out.println("Invalid State. Currently on State S8. Please Start");
     }
 
     @Override
     public void PayCash() {
-
+        System.out.println("Invalid State. Currently on State S8. Please Start");
     }
 
     @Override
     public void PayCredit() {
-
+        System.out.println("Invalid State. Currently on State S8. Please Start");
     }
 
     @Override
     public void Approved() {
-
+        System.out.println("Invalid State. Currently on State S8. Please Start");
     }
 
     @Override
     public void Reject() {
-
+        System.out.println("Invalid State. Currently on State S8. Please Start");
     }
 
     @Override
     public void SelectGas(int g) {
-
+        System.out.println("Invalid State. Currently on State S8. Please Start");
     }
 
     @Override
     public void StartPump() {
-
+        System.out.println("Invalid State. Currently on State S8. Please Start");
     }
 
     @Override
     public void Cancel() {
-
+        System.out.println("Invalid State. Currently on State S8. Please Start");
     }
 
     @Override
     public void Pump() {
-
+        System.out.println("Invalid State. Currently on State S8. Please Start");
     }
 
     @Override
     public void StopPump() {
-
+        System.out.println("Invalid State. Currently on State S8. Please Start");
     }
 
     @Override
     public void CorrectPin() {
-
+        outPutProcessor.DisplayMenu();
+        outPutProcessor.EjectCard();
+        System.out.println("Transaction Approved");
+        mdaefsm.setStates(3);
     }
 
     @Override
@@ -76,6 +79,7 @@ public class S8 extends State{
         else if(mdaefsm.k>max){
             outPutProcessor.WrongPinMsg();
             outPutProcessor.EjectCard();
+            mdaefsm.setStates(0);
         }
     }
 }
